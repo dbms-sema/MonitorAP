498f855a4f52bcadd4e747c4562fcf1c0c97357d
dbms-sema <henrysemakula@gmail.com>
2020-08-04 00:30:43 +0300

    added retrofit

--- app/src/main/java/com/dbmssema/monitorap/HomeFragment.java
+++ app/src/main/java/com/dbmssema/monitorap/HomeFragment.java
@@ -3,10 +3,18 @@ package com.dbmssema.monitorap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dbmssema.monitorap.requests.ServiceGenerator;
+import com.dbmssema.monitorap.requests.responses.ThingSpeakResponse;
 
 
 /**
@@ -14,6 +22,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private TextView smoke, co;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -23,6 +32,31 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        thingsSpeak();
+        //todo while loop
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        smoke=view.findViewById(R.id.txtSmokeNodeOne);
+        co=view.findViewById(R.id.txtCoNodeOne);
+
+        return view;
+    }
+
+    private void thingsSpeak() {
+
+        Call<ThingSpeakResponse> call= ServiceGenerator.getInstance(getActivity()).getMonitorApi().getSensorData();
+
+        call.enqueue(new Callback<ThingSpeakResponse>() {
+            @Override
+            public void onResponse(Call<ThingSpeakResponse> call, Response<ThingSpeakResponse> response) {
+                ThingSpeakResponse data=response.body();
+                smoke.setText(data.getFeeds().get(0).getField1());
+                co.setText(data.getFeeds().get(0).getField2());
+            }
+
+            @Override
+            public void onFailure(Call<ThingSpeakResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
